dc0f47bb83d602d25defe69a482459cb5ad6f736
Aryan Iqbal <aryanikbalgts@gmail.com>
2026-06-11 00:17:07 +0530

    Update noor-ai.js

--- netlify/functions/noor-ai.js
+++ netlify/functions/noor-ai.js
@@ -1,1 +1,48 @@
+exports.handler = async (event) => {
+  try {
+    const { question } = JSON.parse(event.body);
 
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: `You are Noor AI.
+Answer Islamic questions politely.
+Provide Quran or Hadith references whenever possible.
+
+Question: ${question}`
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        answer:
+          data.candidates?.[0]?.content?.parts?.[0]?.text ||
+          "No answer available."
+      })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        answer: "Server error."
+      })
+    };
+  }
+};
